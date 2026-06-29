179d3a2585c59c6e1f9f5c93e87e3c6959859847
Tomas Xavier Santos <oipxtomas@gmail.com>
2026-06-28 21:32:12 -0300

    first steps into adding publising and pubsub architecture with rabbitmq

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"os/signal"
 
+	pubusb "github.com/bootdotdev/learn-pub-sub-starter/internal"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -17,6 +19,15 @@ func main() {
 	}
 	defer conn.Close()
 
+	channel, err := conn.Channel()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pubusb.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, &routing.PlayingState{
+		IsPaused: true,
+	})
+	defer channel.Close()
 	fmt.Println("Connection sucessfull")
 
 	sigchan := make(chan os.Signal)
---
+++ internal/pubsub.go
@@ -0,0 +1,21 @@
+package pubusb
+
+import (
+	"context"
+	"encoding/json"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	message, err := json.Marshal(val)
+	if err != nil {
+		return err
+	}
+
+	ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{
+		ContentType: "application/json",
+		Body:        message,
+	})
+	return nil
+}
