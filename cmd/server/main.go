a32eb2e60ea1af19b9b8a5f2cdf1287ed57cf8d4
Tomas Xavier Santos <tom.xaviersantos@gmail.com>
2026-07-02 22:49:39 -0300

    feat: add game input loop to server for publishing pause/resume signals
    Server now reads commands via gamelogic.GetInput and publishes PlayingState messages to RabbitMQ on “pause” and “resume” input, with a “quit” command to exit the loop.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -25,13 +25,40 @@ func main() {
 	}
 
 	defer conn.Close()
-
 	pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, &routing.PlayingState{
 		IsPaused: true,
 	})
 
 	defer channel.Close()
 	fmt.Println("Connection sucessfull")
+
+	for {
+		input := gamelogic.GetInput()
+		if len(input) == 0 {
+			continue
+		}
+
+		if input[0] == "pause" {
+			log.Println("Sending Pause signal")
+			pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, &routing.PlayingState{
+				IsPaused: true,
+			})
+			continue
+		}
+		if input[0] == "resume" {
+			log.Println("Sending Resume signal")
+			pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, &routing.PlayingState{
+				IsPaused: false,
+			})
+			continue
+		}
+		if input[0] == "quit" {
+			log.Println("Quiting game")
+			break
+		}
+	}
+
+	defer channel.Close()
 
 	sigchan := make(chan os.Signal)
 	signal.Notify(sigchan, os.Interrupt)
